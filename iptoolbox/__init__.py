b031aca0f564b2333721316556a640b18108c8e4
V70024 <Zz2f0024@protonmail.com>
2022-08-30 00:50:59 +0430

    added project urls

--- iptoolbox/__init__.py
+++ iptoolbox/__init__.py
@@ -4,4 +4,6 @@ from . import validation
 __name__ = "iptoolbox"
 __version__ = "0.0.2"
 __description__ = f'{__name__} is a tool for purposefully creating IP, testing and scanning it'
-
+__url_repository__ = f'https://github.com/V70024/{__name__}/tree/{__version__}'
+__url_documentation__ = f'https://github.com/V70024/{__name__}/wiki/{__name__}-{__version__}'
+__url_bog_tracker__ = f'https://github.com/V70024/{__name__}/issues/'
